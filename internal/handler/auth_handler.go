79d25be5daff6c78826be9490572311b9e3db679
SauletTheBest <231003@astanait.edu.kz>
2026-03-13 21:15:17 +0500

    adding auth_handler functions

--- internal/handler/auth_handler.go
+++ internal/handler/auth_handler.go
@@ -4,7 +4,9 @@ package handler
 import (
 	"net/http"
 	"github.com/gin-gonic/gin"
-	"github.com/SauletTheBest/BackendFinancialApplication/internal/usecase"
+	"github.com/SauletTheBest/BackendFinancialApplication/internal/delivery/http/dto"
+	"github.com/SauletTheBest/BackendFinancialApplication/internal/delivery/http/usecase"
+
 )
 
 type AuthHandler struct {
@@ -12,5 +14,29 @@ type AuthHandler struct {
 }
 
 func (h *AuthHandler) Register(c *gin.Context) {
-	var req usecase.RegisterRequest
+	var req dto.RegisterRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H {
+			"error": "invalid request body",
+		})
+		return
+	}
+
+	err := h.authUsecase.Register(c.Request.Context(), req)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H {
+			"error": err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusAccepted, gin.H {
+		"message": "user created",
+	})
+}
+
+func (h *AuthHandler) Login(c *gin.Context) {
+	var req dto.LoginRequest
+
+	if	
 }
